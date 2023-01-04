1659e4fc6650180991cfaa23408fef36d790fb58
Paulo Sergio <paulosergioilha38@gmail.com>
2023-01-04 13:04:40 -0300

    Nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import './App.css'
-import { Link } from 'react-router-dom';
+import "./App.css";
+import { Link, Outlet } from "react-router-dom";
 
 export default function App() {
-  
-  return(
-    <nav
-    style={{
-      borderBottom: "solid 1px",
-      paddingBottom: "1rem",
-    }}
-  >
-    <Link to="/invoices">Invoices</Link> |{" "}
-    <Link to="/expenses">Expenses</Link>
-  </nav>
-
+  return (
+    <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </>
   );
 }
