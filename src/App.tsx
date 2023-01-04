1f13abf7f0ee72d2aa18c18e764c6a70762d82d3
Paulo Sergio <paulosergioilha38@gmail.com>
2023-01-04 11:59:11 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Link } from 'react-router-dom';
 
 export default function App() {
   
   return(
-    <h1>Bookkeeper!</h1>
+    <nav
+    style={{
+      borderBottom: "solid 1px",
+      paddingBottom: "1rem",
+    }}
+  >
+    <Link to="/invoices">Invoices</Link> |{" "}
+    <Link to="/expenses">Expenses</Link>
+  </nav>
 
   );
 }
